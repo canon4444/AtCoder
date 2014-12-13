b00859cd258c5988a13a78d88921d2fccb31ee6c
canon4444 <haruharu724@hotmail.com>
2014-12-13 12:45:28 +0900

    Update CTF_A_D.c
    RE

--- CTF_A/CTF_A_D.c
+++ CTF_A/CTF_A_D.c
@@ -1,24 +1,25 @@
-    #include <stdio.h>
-     
-    int main()
-    {
+#include <stdio.h>
+
+int main()
+{
     int i, j;
-     
+
     int N, Q;
     scanf("%d%d", &N, &Q);
     int a[Q], b[Q], s[Q], t[Q];
     for ( i = 0; i < Q; ++i ) scanf("%d%d%d%d", &a[i], &b[i], &s[i], &t[i]);
-     
+
     int cost;
-    int note[N];
+    int note[Q][N];
+    for ( i = 0; i < Q; ++i ) for ( j = 0; j < N; ++j ) note[i][j] = 0;
     for ( i = 0; i < Q; ++i ) {
-    cost = 0;
-    for ( j = 0; j < N; ++j ) note[j] = 0;
-    for ( j = s[i]-1; j < t[i]-1; ++j ) note[j] = 1;
-    for ( j = a[i]-1; j < b[i]-1; ++j ) note[j] = 0;
-    for ( j = 0; j < N; ++j ) cost += note[j];
-    printf("%d\n", cost * 100);
+        cost = 0;
+        //for ( j = 0; j < N; ++j ) note[j] = 0;
+        for ( j = s[i]-1; j < t[i]-1; ++j ) note[i][j] = 1;
+        for ( j = a[i]-1; j < b[i]-1; ++j ) note[i][j] = 0;
+        for ( j = 0; j < N; ++j ) cost += note[i][j];
+        printf("%d\n", cost * 100);
     }
-     
+
     return 0;
-    }
+}
