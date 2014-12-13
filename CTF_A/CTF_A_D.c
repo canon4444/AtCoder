3da039ca91cad2bcfe7a3ebf24632ed7d65dc4a6
canon4444 <haruharu724@hotmail.com>
2014-12-13 12:41:58 +0900

    Create CTF_A_D.c
    TLE

---
+++ CTF_A/CTF_A_D.c
@@ -0,0 +1,24 @@
+    #include <stdio.h>
+     
+    int main()
+    {
+    int i, j;
+     
+    int N, Q;
+    scanf("%d%d", &N, &Q);
+    int a[Q], b[Q], s[Q], t[Q];
+    for ( i = 0; i < Q; ++i ) scanf("%d%d%d%d", &a[i], &b[i], &s[i], &t[i]);
+     
+    int cost;
+    int note[N];
+    for ( i = 0; i < Q; ++i ) {
+    cost = 0;
+    for ( j = 0; j < N; ++j ) note[j] = 0;
+    for ( j = s[i]-1; j < t[i]-1; ++j ) note[j] = 1;
+    for ( j = a[i]-1; j < b[i]-1; ++j ) note[j] = 0;
+    for ( j = 0; j < N; ++j ) cost += note[j];
+    printf("%d\n", cost * 100);
+    }
+     
+    return 0;
+    }
