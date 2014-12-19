4d8a687a7f995b56bccfc32ed3f9f57242101781
canon4444 <haruharu724@hotmail.com>
2014-12-19 16:46:32 +0900

    Create CTF_B_B.c

---
+++ CTF_B/CTF_B_B.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    int A, B, C;
+    scanf("%d%d%d", &A, &B, &C);
+
+    int max = 0;
+    for ( i = 0; i < 4; ++i )
+        switch ( i ) {
+        case 0: max = max < (A + B) + C ? (A + B) + C : max; break;
+        case 1: max = max < (A + B) * C ? (A + B) * C : max; break;
+        case 2: max = max < (A * B) + C ? (A * B) + C : max; break;
+        case 3: max = max < (A * B) * C ? (A * B) * C : max; break;
+        default: break;
+        }
+
+    printf("%d\n", max);
+
+    return 0;
+}
