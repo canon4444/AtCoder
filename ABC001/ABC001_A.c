1438f44538967006258a4276668d1315ec5b52de
canon4444 <haruharu724@hotmail.com>
2014-12-20 21:18:15 +0900

    Update ABC001_A.c

--- ABC001/ABC001_A.c
+++ ABC001/ABC001_A.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-     
+
 int main()
 {
-    int a, b;
-    scanf("%d%d", &a, &b);
+    int H[2];
+    scanf("%d%d", &H[0], &H[1]);
 
-    printf("%d\n", a - b);
+    printf("%d\n", H[0] - H[1]);
 
     return 0;
 }
