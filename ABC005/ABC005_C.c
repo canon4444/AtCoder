ceae039e613e56a95d25b1adab0e1f2bc622e1d3
canon4444 <haruharu724@hotmail.com>
2014-12-20 21:27:54 +0900

    Update ABC005_C.c

--- ABC005/ABC005_C.c
+++ ABC005/ABC005_C.c
@@ -4,22 +4,18 @@ int main()
 {
     int i, j, k;
 
-    int T, N;
-    scanf("%d%d", &T, &N);
-    int A[N];
-    for ( i = 0; i < N; ++i ) scanf("%d", &A[i]);
-    int M;
-    scanf("%d", &M);
-    int B[M];
-    for ( i = 0; i < M; ++i ) scanf("%d", &B[i]);
+    int T, N; scanf("%d%d", &T, &N);
+    int A[N]; for ( i = 0; i < N; ++i ) scanf("%d", &A[i]);
+    int M; scanf("%d", &M);
+    int B[M]; for ( i = 0; i < M; ++i ) scanf("%d", &B[i]);
      
     int flag = ( N < M ) ? 0 : 1;
     for ( i = 0, k = 0; flag && (i < M); ++i )
         for ( j = k; j < N; k = ++j )
             if ( B[i] < A[j] ) { flag = 0; break; }
-            else if ( A[j] <= B[i] && B[i] <= (A[j] + T) ) { 
+            else if ( A[j] <= B[i] && B[i] <= (A[j] + T) ) { /* can */
                 flag = 1, k = ++j;
-                break; /* can */
+                break;
             } else flag = 0; /* can't */
     
     if ( flag )
