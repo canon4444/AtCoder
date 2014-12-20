89d2511e1eaab1cabb33ccd47ce428fa27ffb919
canon4444 <haruharu724@hotmail.com>
2014-12-20 20:55:00 +0900

    Create ABC001_B.c

---
+++ ABC001/ABC001_B.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    double m;
+    scanf("%lf", &m);
+
+    m /= (double)1000;
+    if ( m < 0.1 ) m = 0;
+    else if ( 0.1 <= m && m <= 5 ) m *= 10;
+    else if ( 6 <= m && m <= 30 )  m += 50;
+    else if ( 35 <= m && m <= 70 ) m = ((m - 30) / 5 + 80);
+    else if ( 70 < m ) m = 89;
+
+    printf("%02.0f\n", m);
+
+    return 0;
+}
