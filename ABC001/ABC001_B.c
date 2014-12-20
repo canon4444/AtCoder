49125c0e9990ae541ea7d0cda81094c33c7d9cf1
canon4444 <haruharu724@hotmail.com>
2014-12-20 21:20:11 +0900

    Update ABC001_B.c

--- ABC001/ABC001_B.c
+++ ABC001/ABC001_B.c
@@ -2,13 +2,12 @@
 
 int main()
 {
-    double m;
-    scanf("%lf", &m);
+    double m; scanf("%lf", &m);
 
-    m /= (double)1000;
+    m /= 1000;
     if ( m < 0.1 ) m = 0;
     else if ( 0.1 <= m && m <= 5 ) m *= 10;
-    else if ( 6 <= m && m <= 30 )  m += 50;
+    else if ( 6 <= m && m <= 30 ) m += 50;
     else if ( 35 <= m && m <= 70 ) m = ((m - 30) / 5 + 80);
     else if ( 70 < m ) m = 89;
 
