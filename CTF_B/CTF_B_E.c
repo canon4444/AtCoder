124991da9c241c5565c102fdb9b5f8f6779aa124
canon4444 <haruharu724@hotmail.com>
2014-12-26 13:24:50 +0900

    Update CTF_B_E.c

--- CTF_B/CTF_B_E.c
+++ CTF_B/CTF_B_E.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+typedef struct {
+    int x, y;
+} axis;
+
 int main()
 {
     int i, j, k;
@@ -9,38 +13,30 @@ int main()
     int s[2], g[2], r[N], c[N], h[N], w[N];
     scanf("%d%d", &s[0], &s[1]);
     scanf("%d%d", &g[0], &g[1]);
-    for ( i = 0; i < 2; ++i ) --s[i], --g[i];
+    for( i = 0; i < 2; ++i ) --s[i], --g[i];
     scanf("%d", &N);
-    for ( i = 0; i < N; ++i ) scanf("%d%d%d%d", &r[i], &c[i], &h[i], &w[i]);
+    for( i = 0; i < N; ++i ) scanf("%d%d%d%d", &r[i], &c[i], &h[i], &w[i]);
 
-    int table[R][C], x = s[1], y = s[0];
-    for ( i = 0; i < R; ++i ) for ( j = 0; j < C; ++j ) table[i][j] = 0;
-    for ( i = 0; i < N; ++i ) for ( j = r[i]-1; j < r[i]+h[i]-1; ++j ) for ( k = c[i]-1; k < c[i]+w[i]-1; ++k ) table[j][k] = 1;
-    
-    int flag = 1, note[4], check = 0;
-    for ( i = 0; flag; ++i ) {
-        if ( y == g[1] && x == g[0] ) { flag = 1; break; }
-        if ( table[y-1][x] && 0 < y ) note[0] = 1; else note[0] = 0;
-        if ( table[y+1][x] && y < R ) note[1] = 1; else note[1] = 0;
-        if ( table[y][x-1] && 0 < x ) note[2] = 1; else note[2] = 0;
-        if ( table[y][x+1] && x < C ) note[3] = 1; else note[3] = 0;
-        int error = 0;
-        for ( j = 0; j < 4; ++j )
-            if ( note[j] ) switch ( j ) {
-                case 0: --y; break;
-                case 1: ++y; break;
-                case 2: --x; break;
-                case 3: ++x; break;
-                default: break;
-                }
-            else ++error;
-        if ( 4 == error ) { x = s[1]; y = s[0]; ++check; }
-        if ( 4 == check ) { flag = 0; break; }
-        table[y][x] = -1;
+    //色を塗る
+    int table[R][C];
+    for( i = 0; i < R; ++i ) for( j = 0; j < C; ++j ) table[i][j] = 0;
+    for( i = 0; i < N; ++i ) for( j = r[i]-1; j < r[i]+h[i]-1; ++j ) for( k = c[i]-1; k < c[i]+w[i]-1; ++k ) table[j][k] = 1;
+    if( !table[s[0]][s[1]] || !table[g[0]][g[1]] ){ printf("NO\n"); return 0; } //スタートとゴールが白だったら
+
+    //幅優先探索
+    axis queue[R*C];
+    int pre = 0, rear = 0;
+    queue[rear].x = s[1], queue[rear].y = s[0];
+    while( pre < R*C && rear < R*C ){
+        int x = queue[pre].x, y = queue[pre++].y;
+        table[y][x] = 0;
+        if( 0 < y && table[y-1][x] ) queue[++rear].x = x, queue[rear].y = y-1, table[y-1][x] = 0;
+        if( y < R-1 && table[y+1][x] ) queue[++rear].x = x, queue[rear].y = y+1, table[y+1][x] = 0;
+        if( 0 < x && table[y][x-1] ) queue[++rear].x = x-1, queue[rear].y = y, table[y][x-1] = 0;
+        if( x < C-1 && table[y][x+1] ) queue[++rear].x = x+1, queue[rear].y = y, table[y][x+1] = 0;
+        if( x == g[1] && y == g[0] ){ printf("YES\n"); return 0; }
     }
-
-    if ( flag ) printf("YES\n");
-    else printf("NO\n");
+    printf("NO\n");
 
     return 0;
 }
