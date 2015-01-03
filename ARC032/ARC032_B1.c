2d4b4b22af8d74dd9189c375283d4ba9081787a9
canon4444 <haruharu724@hotmail.com>
2015-01-04 01:12:13 +0900

    Update ARC032_B1.c

--- ARC032/ARC032_B1.c
+++ ARC032/ARC032_B1.c
@@ -17,10 +17,10 @@ int main()
     for( i = 0; i < M; ++i )
         table[a[i]-1][0] = 1, table[b[i]-1][1] = a[i];
     for( i = 0; i < N; ++i )
-        if( table[i][0] && table[i][1] ) ;
-        else if( table[i][0] ) ++sum;
-        else if( table[i][1] ) ;
-        else ++sum;
+        if( table[i][0] && table[i][1] ) ; //2個以上のノードと結合
+        else if( table[i][0] ) ++sum; //+かつ端のノード
+        else if( table[i][1] ) ; //-かつ端のノード
+        else ++sum; //孤立ノード
 
     //出力
     printf("%d\n", sum-1);
