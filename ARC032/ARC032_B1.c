9a8362b480d186a346a89879a89c38c15b5da944
canon4444 <haruharu724@hotmail.com>
2015-01-04 01:06:26 +0900

    Create ARC032_B1.c
    WA -> 15, 19

---
+++ ARC032/ARC032_B1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+
+    //入力
+    int N, M; scanf("%d%d", &N, &M);
+    int a[M], b[M];
+    for( i = 0; i < M; ++i ) scanf("%d%d", &a[i], &b[i]);
+
+    //みょん
+    //15, 19
+    int table[N][2], sum = 0;
+    for( i = 0; i < N; ++i )
+        table[i][0] = 0, table[i][1] = 0;
+    for( i = 0; i < M; ++i )
+        table[a[i]-1][0] = 1, table[b[i]-1][1] = a[i];
+    for( i = 0; i < N; ++i )
+        if( table[i][0] && table[i][1] ) ;
+        else if( table[i][0] ) ++sum;
+        else if( table[i][1] ) ;
+        else ++sum;
+
+    //出力
+    printf("%d\n", sum-1);
+
+    return 0;
+}
