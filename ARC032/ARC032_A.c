c9478b55bc58d73ba2c2965800fa2edddf963d22
canon4444 <haruharu724@hotmail.com>
2015-01-03 22:51:01 +0900

    Update ARC032_A.c

--- ARC032/ARC032_A.c
+++ ARC032/ARC032_A.c
@@ -7,13 +7,13 @@ int main()
     int n; scanf("%d", &n);
 
     int sum = 0, flag = 0, memo = 0;
-    for( i = 0; i <= n; ++i ) sum += i;
+    for( i = 0; i <= n; ++i ) sum += i; //1+ ... + n = sum
 
-    for( i = 0; i <= 1000; ++i ) memo += i;
+    for( i = 0; i <= 1000; ++i ) memo += i; //下のループの上限
 
     for( i = 2; i <= memo; ++i ) {
-        if( sum % i == 0 ) ++flag;
-        if( 1 < flag ) break;
+        if( sum % i == 0 ) ++flag; //約数がある
+        if( 1 < flag ) break; //約数が2つ以上で非素数（1では既に割っている）
     }
 
     if( 1 == flag ) printf("WANWAN\n");
