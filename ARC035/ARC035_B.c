9c5eef047909e7ca33fead1848d7d0269c155a80
canon4444 <haruharu724@hotmail.com>
2015-03-07 22:32:36 +0900

    Create ARC035_B.c

---
+++ ARC035/ARC035_B.c
@@ -0,0 +1,40 @@
+//50点解答
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j, tmp;
+
+    int N;
+    scanf("%d", &N);
+    int T[N];
+    for( i = 0; i < N; ++i )
+        scanf("%d", &T[i]);
+
+    for( i = 0; i < N; ++i )
+        for( j = i+1; j < N; ++j )
+            if( T[i] > T[j] ){
+                tmp = T[i];
+                T[i] = T[j];
+                T[j] = tmp;
+            }
+
+    int time = 0;
+    long penalty = 0;
+    for( i = 0; i < N; ++i ){
+        time += T[i];
+        penalty += time;
+    }
+
+    int kind = 1, cnt;
+    for( i = 0, cnt = 1; i < N; cnt = 0, ++i, cnt = 1 ){
+        while( T[i] == T[i+cnt] ) ++cnt;
+        kind *= cnt;
+        kind %= 1000000007;
+    }
+
+    printf("%ld\n%d\n", penalty, kind);
+
+    return 0;
+}
