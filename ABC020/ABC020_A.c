60aed465273a345b40d18369be899ee0e7d0f1c0
canon4444 <haruharu724@hotmail.com>
2015-03-27 09:26:48 +0900

    Create ABC020_A.c

---
+++ ABC020/ABC020_A.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int Q;
+    scanf("%d", &Q);
+
+    switch( Q ){
+    case 1: printf("ABC\n"); break;
+    case 2: printf("chokudai\n"); break;
+    }
+
+    return 0;
+}
