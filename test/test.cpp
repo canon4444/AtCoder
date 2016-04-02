6b9e0d2ce2ada2162076391c5adc4947a12e33fd
canon4444 <haruharu724@hotmail.com>
2016-04-02 22:43:50 +0900

    Create test.cpp

---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+// test.cpp : コンソール アプリケーションのエントリ ポイントを定義します。
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!" << endl;
+    return 0;
+}
+
