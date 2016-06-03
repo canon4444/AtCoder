16da2dd4f2770416936ce18d70d55ab72ad62661
canon4444 <canon4444@users.noreply.github.com>
2016-06-03 22:39:20 +0900

    Create ARC055_A.cpp

---
+++ ARC055/ARC055_A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+    
+    cout << 1;
+    for( int i = 0; i < N-1; ++i ){
+        cout << 0;
+    }
+    cout << 7 << endl;
+    
+    return 0;
+}
