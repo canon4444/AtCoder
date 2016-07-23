17583949cebbd29f0e538280e111da7ed9dd3027
canon4444 <canon4444@users.noreply.github.com>
2016-07-23 23:11:36 +0900

    Create A.cpp

---
+++ ABC-ARC/ABC042-ARC058/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+
+
+    if( A + B + C == 17 ) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+
+    return 0;
+}
