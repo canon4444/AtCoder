c7a9e7c6e6481d9a2e6b8cb0ec9a3378f969b75c
canon4444 <canon4444@users.noreply.github.com>
2016-07-24 22:51:05 +0900

    Create A.cpp

---
+++ ABC/ABC006/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    int N;
+    cin >> N;
+
+
+    if( N % 3 == 0 ) cout << "YES" << endl;
+    else cout << "NO" << endl;
+
+
+    return 0;
+}
