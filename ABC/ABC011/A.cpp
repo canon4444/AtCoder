be7c8b76779c6a0d39a216859208deb5e527f12a
canon4444 <canon4444@users.noreply.github.com>
2016-08-01 20:43:49 +0900

    Create A.cpp

---
+++ ABC/ABC011/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+
+using namespace std;
+
+
+int main()
+{
+    int N;
+    cin >> N;
+
+
+    if( N < 12 ) cout << N + 1 << endl;
+    else cout << 1 << endl;
+
+
+    return 0;
+}
