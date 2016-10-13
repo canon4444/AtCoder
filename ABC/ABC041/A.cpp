b112dc5f2f2ed7ae9064472e1f46272cf5ea7bcc
canon4444 <canon4444@users.noreply.github.com>
2016-10-14 01:40:32 +0900

    Create A.cpp

---
+++ ABC/ABC041/A.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    string s;
+    cin >> s;
+    
+    int i;
+    cin >> i;
+    
+    
+    cout << s.c_str()[i-1] << endl;
+    
+    
+    return 0;
+}
