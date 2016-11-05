3f91f81b262a277b7ad8a923e4a03a4a5b0d3d6a
canon4444 <canon4444@users.noreply.github.com>
2016-11-06 01:37:06 +0900

    Create A.cpp

---
+++ etc./DDCC2016-qual/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+ 
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    double A, B, C;
+    cin >> A >> B >> C;
+    
+    
+    printf("%lf\n", (C/A)*B);
+    
+    
+    return 0;
+}
