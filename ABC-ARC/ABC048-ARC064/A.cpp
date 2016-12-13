ababee75953b8569bd0b8b48903a1e84642a8bac
canon4444 <canon4444@users.noreply.github.com>
2016-12-13 20:43:53 +0900

    Create A.cpp

---
+++ ABC-ARC/ABC048-ARC064/A.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+
+int main()
+{
+    string a, s, c;
+    cin >> a >> s >> c;
+
+
+    cout << "A" << char(s.c_str()[0]) << "C" << endl;
+
+
+    return 0;
+}
