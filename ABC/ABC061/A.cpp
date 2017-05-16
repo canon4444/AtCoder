a0cd8da232db7c93baf75d974b2aa6b5bacca324
haruka-t <haruka-t@naist-wavenet124-202.naist.jp>
2017-05-16 11:39:29 +0900

    Creat A.cpp;2C

---
+++ ABC/ABC061/A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+ 
+int main()
+{
+    int A, B, C;
+    cin >> A >> B >> C;
+    
+    if( A <= C && C <= B ) cout << "Yes" << endl;
+    else cout << "No" << endl;
+ 
+    return 0;
+}
