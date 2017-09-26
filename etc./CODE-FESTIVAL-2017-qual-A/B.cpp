ffd014695b9cf650b0e14d21cad6b7ba1c3d070d
canon4444 <haruharu724@hotmail.com>
2017-09-26 13:46:52 +0900

    こどふぇす予選A

---
+++ etc./CODE-FESTIVAL-2017-qual-A/B.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int N, M, K;
+    cin >> N >> M >> K;
+
+
+    int ans = 0;
+    int min = 0, max = N*M, mid = (min+max)/2;
+
+    if( K%N == 0 || K%M == 0 ){
+        cout << "Yes" << endl;
+    } else {
+        while( 1 )
+
+
+
+    return 0;
+}
