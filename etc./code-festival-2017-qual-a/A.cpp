57793d78244120c9ad4e6742eab6833af78c1ccd
canon4444 <haruharu724@hotmail.com>
2017-09-26 13:45:54 +0900

    こどふぇす予選A A.cpp

---
+++ etc./code-festival-2017-qual-a/A.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string S;
+    cin >> S;
+
+    if( S.find("YAKI") == 0 ) cout << "Yes" << endl;
+    else cout << "No" << endl;
+
+    return 0;
+}
