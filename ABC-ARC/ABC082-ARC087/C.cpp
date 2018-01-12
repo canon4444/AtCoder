97eb7ccd13744416f2035c3e61c810190539fa96
canon4444 <haruharu724@hotmail.com>
2018-01-12 20:58:45 +0900

    途中

---
+++ ABC-ARC/ABC082-ARC087/C.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cin >> N;
+
+    long long int a[(int)1e5+10] = { 0 };
+    for( int i = 0; i < N; ++i ) cin >> a[i];
+
+
+
+    return 0;
+}
