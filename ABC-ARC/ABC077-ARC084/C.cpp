797f72364c46320e278d0a79acc09f749b646bc7
canon4444 <haruharu724@hotmail.com>
2018-07-27 14:06:47 +0900

    2018-07-27 14:06:47 +0900

--- ABC-ARC/ABC077-ARC084/C.cpp
+++ ABC-ARC/ABC077-ARC084/C.cpp
@@ -16,42 +16,27 @@ int main()
     for( int i = 0; i < N; ++i )
         cin >> C[i];
 
-    int ans = 0;
-
     sort(A.begin(), A.end());
-    sort(B.begin(), B.end());
+    // sort(B.begin(), B.end());
     sort(C.begin(), C.end());
 
-    vector<long long int> a(N), c(N);
+    int ans = 0;
+    vector<long long int>::iterator max, min;
+    int above, below;
+
     for( int i = 0; i < N; ++i ){
-        a[i] = 0;
-        for( int j = 0; j < N; ++j ){
-            if( A[i] < B[j] ){
-                a[i] = N - j;
-                break;
-            }
-        }
+        // B[i]に対する上段を二分探索
+        min = lower_bound(A.begin(), A.end(), B[i]);
+        above = distance(A.begin(), min);
+
+        // B[i]に対する下段を二分探索
+        max = upper_bound(C.begin(), C.end(), B[i]);
+        below = N - distance(C.begin(), max);
+
+        ans += above * below;
+
+        // cout << B[i] << " : " << above << ", " << below << endl;
     }
-    for( int i = 0; i < N; ++i ){
-        b[i] = 0;
-        for( int j = N-b[i]-1; j < N; ++j ){
-            if( B[i] < C[j] ){
-                b[i] -= j + 1;
-                break;
-            }
-        }
-    }    
-    /*
-    cout << "----" << endl;
-    for( int i = 0; i < N; ++i ) cout << a[i] << " ";
-    cout << endl;
-    for( int i = 0; i < N; ++i ) cout << b[i] << " ";
-    cout << endl;
-    cout << "----" << endl;
-    */
-    for( int i = 0; i < N; ++i )
-        for( int j = N-a[i]; j < N; ++j ){
-            ans += b[j];
 
     cout << ans << endl;
 
---
+++ ABC-ARC/ABC077-ARC084/rand.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+int main()
+{
+    srand(time(0));
+
+    int N = rand() % 10 + 1;
+
+    return 0;
+}
