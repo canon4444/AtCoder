97097b6c1ac60d717079762f5a06b0e1c7429a6c
canon4444 <haruharu724@hotmail.com>
2018-09-26 11:57:29 +0900

    両端の文字と同じ文字から消していけば解けそう？

--- ABC-ARC/ABC048-ARC064/D.cpp
+++ ABC-ARC/ABC048-ARC064/D.cpp
@@ -1,5 +1,24 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+    string s;
+    cin >> s;
+
+    int ans = 1;
+    int len;
+
+    while( 1 )
+        len = s.length();
+
+        if( len < 3 ){
+            if( ans == 1 ) cout << "Second" << endl;
+            else cout << "First" << endl;
+            return 0;
+        }
+
+        for( int i = 1; i < len-1; ++i ){
+            if( s[0] == s[i] ){
+            }
