592d508df00bd7398dea9ba0aa651665e5fafc59
canon4444 <haruharu724@hotmail.com>
2018-09-29 22:53:38 +0900

    ABC-ARC/ABC048-ARC064/D.cpp

--- ABC-ARC/ABC048-ARC064/D.cpp
+++ ABC-ARC/ABC048-ARC064/D.cpp
@@ -10,15 +10,20 @@ int main()
     int ans = 1;
     int len;
 
-    while( 1 )
+    while( 1 ){
         len = s.length();
 
+        // 2文字以下だと，その手番の人の負け
         if( len < 3 ){
             if( ans == 1 ) cout << "Second" << endl;
             else cout << "First" << endl;
             return 0;
         }
 
+        // 端と同じ文字から取っていく
         for( int i = 1; i < len-1; ++i ){
-            if( s[0] == s[i] ){
+            if( s[0] == s[i] || s[len-1] == s[i] ){
+                s.erase(s.begin() + i);
+            } else {
             }
+        }
