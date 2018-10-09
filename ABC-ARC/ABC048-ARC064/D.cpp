7416e3d0f1b30da6c28fd5956f87f71d07ca220c
canon4444 <haruharu724@hotmail.com>
2018-10-09 16:20:43 +0900

    TLE https://beta.atcoder.jp/contests/abc048/tasks/arc064_b

--- ABC-ARC/ABC048-ARC064/D.cpp
+++ ABC-ARC/ABC048-ARC064/D.cpp
@@ -11,24 +11,53 @@ int main()
     bool flag = true;
 
     while( 1 ){
-        // 2文字以下だと，その手番の人の負け
+        // 2文字以下の場合，その手番の人の負け
         if( s.length() < 3 ){
             if( ans == 1 ) cout << "Second" << endl;
             else cout << "First" << endl;
             return 0;
         }
 
+        // まだ取れる文字があるかどうかチェック
+        flag = false;
+        for( int i = 1; i < s.length()-1; ++i ){
+            if( s[i-1] != s[i+1] ){
+                flag = true;
+                break;
+            }
+        }
+
+        // 取れる文字がない場合，その手番の人の負け
+        if( !flag ){
+            if( ans == 1 ) cout << "Second" << endl;
+            else cout << "First" << endl;
+            return 0;
+        }
+
         // 端と同じ文字から取っていく
-        for( int i = 1; i < s.length()-1 && flag; ++i ){
-            if( s[0] == s[i] || s[s.length()-1] == s[i] ){
-                s.erase(s.begin() + i);
-                flag = false;
+        for( int i = 1; i < s.length()-1; ++i ){
+            if( s[0] == s[i] || s[s.length()-1] == s[i] ){ // 端と同じ文字
+                if( s[i-1] != s[i+1] ){ // 取った後に文字が隣合わない
+                    s.erase(s.begin() + i);
+                    flag = false;
+                    break;
+                }
             }
         }
 
         // 端と同じ文字がなく，取っていない場合は適当に取る
-        if( flag )
-            s.erase(s.begin() + 1);
+        if( flag ){
+            for( int i = 1; i < s.length()-1; ++i ){
+                if( s[i-1] != s[i+1] ){ // 取った後に文字が隣合わない
+                    s.erase(s.begin() + i);
+                    break;
+                }
+            }
+        }
 
-        ans = ans == 1 ? 
+        flag = true;
+        ans = ans == 1 ? 2 : 1;
     }
+    
+    return 0;
+}
