94a08458ac68c7d45d2a439860113eba6be31ad4
hat <canon4444@users.noreply.github.com>
2020-05-31 03:22:58 +0900

    Update ABC162.cpp

--- ABC/ABC162.cpp
+++ ABC/ABC162.cpp
@@ -5,9 +5,11 @@ using namespace std;
  
 void A()
 {
+    // 入力
     string N;
     cin >> N;
     
+    // 出力
     if( N.find("7") != string::npos )
         cout << "Yes" << endl;
     else
@@ -16,15 +18,18 @@ void A()
  
 void B()
 {
+    // 入力
     int N;
     cin >> N;
     
     long long int ans = 0;
     
+    // "Fizz", "Buzz", "FizzBuzz"のいずれでもない場合を合算する
     for( int i = 1; i < N+1; ++i )
         if( (i % 3 != 0) && (i % 5 != 0) )
             ans += i;
-    
+ 
+    // 出力
     cout << ans << endl;
 }
  
@@ -48,16 +53,19 @@ long long int gcd(long long int a, long long int b)
  
 void C()
 {
+    // 入力
     int K;
     cin >> K;
     
     int ans = 0;
     
+    // 2重GCDで解ける
     for( int a = 1; a < K+1; ++a )
         for( int b = 1; b < K+1; ++b )
             for( int c = 1; c < K+1; ++c )
                 ans += gcd(gcd(a, b), c);
-    
+
+ // 出力
     cout << ans << endl;
 }
  
@@ -68,127 +76,32 @@ void D()
     string S;
     cin >> N >> S;
     
-    // SをR, G, Bに分割する
-    vector<int> R, G, B;
+    // R, G, Bの個数を数える
+    // int型だとansに代入する際に桁漏れするので、あらかじめlong long int型にするか、代入するときにキャストする
+    // long long int R = 0, G = 0, B = 0;
+    int R = 0, G = 0, B = 0;
     for( int i = 0; i < N; ++i )
         switch( S[i] ){
-            case 'R': R.push_back(i+1); break;
-            case 'G': G.push_back(i+1); break;
-            case 'B': B.push_back(i+1); break;
+            case 'R': ++R; break;
+            case 'G': ++G; break;
+            case 'B': ++B; break;
         }
     
-    long long int ans = 0;
-    int lenR = (int)R.size(),
-        lenG = (int)G.size(),
-        lenB = (int)B.size();
- 
-    // Si != Sj && Sj != Sk && Sk != Si
-    // j - i != k - j
-    // 1 <= i < j < k <= N
-    /*
-    for( int i = 0; i < N; ++i ){
-        if( S[i] == 'R' ){
-            // R[i], G[j], B[k]
-            for( int j = 0; j < lenG; ++j ){
-                if( G[j] < R[i] ) continue;
-                for( int k = 0; k < lenB; ++k ){
-                    if( B[k] < G[j] ) continue;
-                    if( G[j]-R[i] == B[k]-G[j] ) continue;
-                    ++ans;
-                    cout << "R[i], G[j], B[k]" << endl;
-                }
-            }
-            // R[i], B[j], G[k]
-            for( int j = 0; j < lenB; ++j ){
-                if( B[j] < R[i] ) continue;
-                for( int k = 0; k < lenG; ++k ){
-                    if( G[k] < B[j] ) continue;
-                    if( B[j]-R[i] == G[k]-B[j] ) continue;
-                    ++ans;
-                    cout << "R[i], B[j], G[k]" << endl;
-                }
-            }
-        } else if( S[i] == 'G' ){
-            // G[i], R[j], B[k]
-            for( int j = 0; j < lenR; ++j ){
-                if( R[j] < G[i] ) continue;
-                for( int k = 0; k < lenB; ++k ){
-                    if( B[k] < R[j] ) continue;
-                    if( R[j]-G[i] == B[k]-R[j] ) continue;
-                    ++ans;
-                    cout << "G[i], R[j], B[k]" << endl;
-                }
-            }
-            // G[i], B[j], R[k]
-            for( int j = 0; j < lenB; ++j ){
-                if( B[j] < G[i] ) continue;
-                for( int k = 0; k < lenR; ++k ){
-                    if( R[k] < B[j] ) continue;
-                    if( B[j]-G[i] == R[k]-B[j] ) continue;
-                    ++ans;
-                    cout << "G[i], B[j], R[k]" << endl;
-                }
-            }
-        } else if( S[i] == 'B' ){
-            // B[i], R[j], G[k]
-            for( int j = 0; j < lenR; ++j ){
-                if( R[j] < B[i] ) continue;
-                for( int k = 0; k < lenG; ++k ){
-                    if( G[k] < R[j] ) continue;
-                    if( R[j]-B[i] == G[k]-R[j] ) continue;
-                    ++ans;
-                    cout << "B[i], R[j], G[k]" << endl;
-                }
-            }
-            // B[i], G[j], R[k]
-            for( int j = 0; j < lenG; ++j ){
-                if( G[j] < B[i] ) continue;
-                for( int k = 0; k < lenR; ++k ){
-                    if( R[k] < G[j] ) continue;
-                    if( G[j]-B[i] == R[k]-G[j] ) continue;
-                    ++ans;
-                    cout << "B[i], G[j], R[k]" << endl;
-                }
-            }
-        }
-    }
-    */
-    for( int i = 0; i < lenR; ++i ){
-        // R[i], G[j], B[k]
-        for( int j = 0; j < lenG; ++j ){
-            if( G[j] < R[i] ) continue;
-            for( int k = 0; k < lenB; ++k ){
-                if( B[k] < G[j] ) continue;
-                if( G[j]-R[i] == B[k]-G[j] ) continue;
-                ++ans;
-            }
-        }
-        // R[i], B[j], G[k]
-        for( int j = 0; j < lenB; ++j ){
-            if( B[j] < R[i] ) continue;
-            for( int k = 0; k < lenG; ++k ){
-                if( G[k] < B[j] ) continue;
-                if( B[j]-R[i] == G[k]-B[j] ) continue;
-                ++ans;
-            }
-        }
-    }
-    for( int i = 0; i < lenG; ++i ){
-        // G[i], R[j], B[k]
-        for( int j = 0; j < lenR; ++j ){
-            if( R[j] < G[i] ) continue;
-            for( int k = 0; k < lenB; ++k ){
-                if( B[k] < R[j] ) continue;
-                if( R[j]-G[i] == B[k]-R[j] ) continue;
-                ++ans;
-            }
-        }
-        // G[i], B[j], R[k]
-        
+    // {R, G, B}の組合せの数
+    // 必ずR, G, Bそれぞれひとつずつの組合せになるが、i < j < k順に並びかえればいいので、それぞれの個数をかけた値になる
+    long long int ans = (long long int)R * G * B;
 
-    // B[i], R[j], G[k]
-    // B[i], G[j], R[k]
+    // (j − i) == (k − j)の組合せの数を引く
+    // i, jが決まればkを求められる
+    for( int i = 1; i < N+1; ++i )
+        for( int j = i+1; j < N+1; ++j ){
+            int k = 2*j - i;
+            if( k < j || N < k ) continue;
+            if( S[i-1] != S[j-1] && S[j-1] != S[k-1] && S[k-1] != S[i-1] )
+                --ans;
+        }
     
+    // 出力
     cout << ans << endl;   
 }
  
